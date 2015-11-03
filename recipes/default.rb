f9314d05a3ad138dc52444c585cba58d11ab1960
Daniel Huesch <huesch@amazon.com>
2015-11-03 08:59:27 +0100

    Use latest working recipe version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,20 @@
 app = search(:aws_opsworks_app).first
 
-application app["shortname"] do
-  path "/srv/#{app['shortname']}"
-  packages ["git"]
+execute "ssh-keyscan -H github.com >> /root/.ssh/known_hosts"
 
-  repository app["app_source"]["url"]
+application "/srv/#{app['shortname']}" do
+  javascript "4"
 
-  nodejs do
-    npm false
-    entry_point "server.js"
+  file "/root/.ssh/id_rsa" do
+    content app["app_source"]["ssh_key"]
+    mode 0600
   end
+
+  git "/srv/#{app['shortname']}" do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+  end
+
+  npm_install
+  npm_start
 end
