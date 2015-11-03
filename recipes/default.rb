c275dc5140ee05094b060a6b2c1596bd873159ba
Daniel Huesch <huesch@amazon.com>
2015-11-03 16:58:04 +0100

    Switch deployment from Git repo to tarball on S3

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,19 +1,15 @@
 app = search(:aws_opsworks_app).first
 
-execute "ssh-keyscan -H github.com >> /root/.ssh/known_hosts"
-
-application "/srv/#{app['shortname']}" do
+app_path = "/srv/#{app['shortname']}"
+application app_path do
   javascript "4"
 
-  file "/root/.ssh/id_rsa" do
-    content app["app_source"]["ssh_key"]
-    mode 0600
+  app_tarball_path = ::File.join(Chef::Config[:file_cache_path], "#{app['shortname']}.tar.gz")
+  remote_file app_tarball_path do
+    source app["app_source"]["url"]
   end
 
-  git "/srv/#{app['shortname']}" do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
-  end
+  execute "tar xfz #{app_tarball_path} -C #{app_path} --strip-components=1"
 
   npm_install
   npm_start
