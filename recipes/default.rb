4ecaf7bb866f8e0cea13357aeb6f9845263fc48a
Daniel Huesch <huesch@amazon.com>
2015-11-06 09:04:06 +0100

    Install git, with force where needed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,14 @@
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
+package "git" do
+  # workaround for:
+  # WARNING: The following packages cannot be authenticated!
+  # liberror-perl
+  # STDERR: E: There are problems and -y was used without --force-yes
+  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+end
+
 application app_path do
   javascript "4"
   environment.update("PORT" => "80")
